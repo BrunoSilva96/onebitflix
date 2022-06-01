c2143b32133dd3c7db26d00ffa7de67e5ccbbdb7
Bruno Silva <brunosilvabrait@gmail.com>
2022-06-01 18:23:03 -0300

    Relacionamento entre models

---
+++ src/adminjs/resources/courses.ts
@@ -0,0 +1,9 @@
+import { ResourceOptions } from "adminjs";
+
+export const courseResourceOptions: ResourceOptions = {
+   navigation: 'Catálogo',
+   editProperties: ['name', 'synopsis', 'uploadThumbnail', 'feature', 'categoryId'],
+   filterProperties: ['name', 'synopsis', 'feature', 'categoryId', 'createdAt', 'updatedAt'],
+   listProperties: ['id', 'name', 'feature', 'categoryId'],
+   showProperties: ['id', 'name', 'synopsis', 'feature', 'thumbnailUrl', 'categoryId', 'createdAt', 'updatedAt']
+}
---
+++ src/models/Course.ts
@@ -0,0 +1,47 @@
+import { sequelize } from "../database";
+import { DataTypes, Model, Optional } from "sequelize";
+
+export interface CourseAttributes {
+   id: number
+   name: string
+   synopsis: string
+   thumbnailUrl: string
+   feature: boolean
+   categoryId: number
+}
+
+export interface CourseCreationAttributes extends Optional<CourseAttributes, 'id' | 'thumbnailUrl' | 'feature' > {}
+
+export interface CourseInstance extends Model<CourseAttributes, CourseCreationAttributes>, CourseAttributes{}
+
+export const Course = sequelize.define<CourseInstance, CourseAttributes>('courses', {
+   id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    synopsis: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    thumbnailUrl: {
+      type: DataTypes.STRING
+    },
+    feature: {
+      defaultValue: false,
+      type: DataTypes.BOOLEAN
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: { model: 'categories', key: 'id'},
+      onUpdate: 'CASCADE',
+      onDelete: 'RESTRICT'
+    },
+})
+   
