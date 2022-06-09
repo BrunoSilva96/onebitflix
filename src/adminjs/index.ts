cf51804565fb951e7fbf3a30e7443a9732294b86
Bruno Silva <brunosilvabrait@gmail.com>
2022-06-09 16:13:23 -0300

    Adicionando traduções

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,6 +5,7 @@ import { sequelize } from "../database";
 import { adminJsResources } from "./resources"
 import { User } from "../models";
 import bcrypt from "bcrypt"
+import { locale } from "./locale";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -31,7 +32,8 @@ export const adminJs = new AdminJS({
             hoverBg: '#151515',
          }
       }
-   }
+   },
+   locale: locale
 })
 
 export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
