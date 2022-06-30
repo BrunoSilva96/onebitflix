1d044002ef35e366f94ac13c2fbb274130342ef7
Bruno Silva <brunosilvabrait@gmail.com>
2022-06-30 17:21:36 -0300

    Obtendo Cursos em lançamento

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
-   //GET /courses/featured
+   //GET   /courses/featured
    featured: async (req: Request, res: Response) => {
-
 
       try {
          const featuredCourses = await courseService.getRandomFeaturedCourses()
@@ -16,7 +15,19 @@ export const coursesController = {
       }
    },
 
-   //GET /courses/:id
+   //GET   /courses/newest
+   newest: async (req: Request, res: Response) => {
+      try {
+        const newestCourses = await courseService.getTopTenNewest()
+        return res.json(newestCourses  )
+      } catch (err) {
+         if (err instanceof Error) {
+            return res.status(400).json({ message: err.message })
+         }
+      }
+   },
+
+   //GET   /courses/:id
    show: async (req: Request, res: Response) => {
       const { id } = req.params
 
